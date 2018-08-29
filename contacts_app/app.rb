a0591bdda04d1074a8acc667f8ea3c7f593cac13
Jbrow95 <browningjustin95@gmail.com>
2018-08-29 19:50:21 -0400

    started styling profile page

--- contacts_app/app.rb
+++ contacts_app/app.rb
@@ -1,10 +1,19 @@
 require 'sinatra'
 # require_relative 'calc.rb'
-load './local_env.rb' if File.exist?('./local_env.rb')
+# load './local_env.rb' if File.exist?('./local_env.rb')
 
 enable :sessions
 
 get '/' do
-
     erb :dashboard
+end
+
+post '/account' do
+
+	redirect '/profile'
+end
+
+get '/profile' do
+
+	erb :profile
 end
