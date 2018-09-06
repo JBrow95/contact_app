48eded96458a4a60e1522f934c832626d26c5187
Jbrow95 <browningjustin95@gmail.com>
2018-09-06 15:57:27 -0400

    updated database and fixed some errors

--- contacts_app/app.rb
+++ contacts_app/app.rb
@@ -1,12 +1,18 @@
 require 'sinatra'
 require 'pg'
+require 'securerandom'
 enable :sessions
 
 load 'local_ENV.rb' if File.exist?('local_ENV.rb')
 # conn = PG::Connection.open(:user => ENV['dbuser'], :password => ENV['dbpass'], :dbname => ENV['dbname'], :host => ENV['hostid'], :port => ENV['portid'])
 
+def prepare_statements(conn)
+	conn.prepare("newuser4", "insert into user_info (uuid, user_id, user_pass) values($1, $2, $3)")
+end
+prepare_statements(conn)
 get '/' do
-    erb :dashboard
+	
+    erb :dashboard, locals:{msg: "", error: ""}
 end
 
 post '/account' do
@@ -15,10 +21,14 @@ post '/account' do
 end
 
 post '/newuser' do
-	username = params[:user]
-	# conn.prepare("newuser1", "insert into info_table (uuid, usernames, pass) values($1, $2, $3)")
-	# conn.exec_prepared('newuser1', ["abc", "jbrow43", "abc123"])
-	redirect '/profile'
+	conn.exec_prepared('newuser4', [SecureRandom.uuid, params[:new_user] , params[:new_pass]])
+	if params[:new_user] == 
+		error = "Username Taken"
+		erb :dashboard, locals:{error: "Username Taken"}
+	else
+	msg = "Account Created"
+	erb :dashboard, locals:{msg: "Account Created"}
+	end
 end
 
 get '/profile' do
