af2f8e7e046272ea11f31249b76bb0f4baed9290
Jbrow95 <browningjustin95@gmail.com>
2018-09-11 12:12:34 -0400

    updated modal and get the form to send and grab data from db and autopoulate in erb table

--- contacts_app/app.rb
+++ contacts_app/app.rb
@@ -4,34 +4,52 @@ require 'securerandom'
 enable :sessions
 
 load 'local_ENV.rb' if File.exist?('local_ENV.rb')
-# conn = PG::Connection.open(:user => ENV['dbuser'], :password => ENV['dbpass'], :dbname => ENV['dbname'], :host => ENV['hostid'], :port => ENV['portid'])
+conn = PG::Connection.open(:user => ENV['dbuser'], :password => ENV['dbpass'], :dbname => ENV['dbname'], :host => ENV['hostid'], :port => ENV['portid'])
 
 def prepare_statements(conn)
 	conn.prepare("newuser4", "insert into user_info (uuid, user_id, user_pass) values($1, $2, $3)")
+	conn.prepare("cons", "insert into contact_info (names, address, phone, owner) values($1, $2, $3, $4)")
 end
 prepare_statements(conn)
+
 get '/' do
-	
+
     erb :dashboard, locals:{msg: "", error: ""}
 end
 
 post '/account' do
-
-	redirect '/profile'
+	user = params[:user]
+	pass = params[:pass]
+	res = conn.exec("SELECT * FROM user_info WHERE user_id = '#{user}' AND user_pass = '#{pass}' ")
+	res.each do |n|
+		session[:id] = n['uuid']
+		session[:user_name] = n['user_id']
+		redirect '/profile'
+	end
+		erb :dashboard, locals:{error: "", msg: ""}
 end
 
 post '/newuser' do
 	conn.exec_prepared('newuser4', [SecureRandom.uuid, params[:new_user] , params[:new_pass]])
-	if params[:new_user] == 
-		error = "Username Taken"
-		erb :dashboard, locals:{error: "Username Taken"}
-	else
+	
 	msg = "Account Created"
 	erb :dashboard, locals:{msg: "Account Created"}
-	end
 end
 
 get '/profile' do
+	res = conn.exec("SELECT * FROM contact_info WHERE owner = '#{session[:id]}'")
+	res_arr = []
+	res.each do |r|
+		res_arr << r
+	end
+	username = session[:user_name]
+	erb :profile, locals:{username: username, res_arr: res_arr}
+end
 
-	erb :profile
+post '/contacts' do
+	person = params[:person]
+	address = params[:address]
+	phone = params[:phone]
+	conn.exec_prepared('cons', [params[:person], params[:address], params[:phone], session[:id]])
+	redirect '/profile'
 end
--- contacts_app/public/js/new.js
+++ contacts_app/public/js/new.js
@@ -1,4 +1,21 @@
-function newcon {
+
 	var modal = document.getElementById('myModal');
-	var span = document.getElementById('close')[0];
+	var btn = document.getElementById("myBtn");
+	var span = document.getElementsByClassName('close')[0];
+
+	btn.onclick = function() {
+		modal.style.display = "block";
+	}
+
+
+span.onclick = function() {
+	modal.style.display = "none";
 }
+
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+
