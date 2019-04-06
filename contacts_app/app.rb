ed1a0050ce788d33174d817106a6fd5dac6d8176
Jbrown95 <browningjustin95@gmail.com>
2019-04-05 21:00:20 -0500

    updated for heroku

--- contacts_app/app.rb
+++ contacts_app/app.rb
@@ -4,13 +4,13 @@ require 'securerandom'
 enable :sessions
 
 load 'local_ENV.rb' if File.exist?('local_ENV.rb')
-conn = PG::Connection.open(:user => ENV['dbuser'], :password => ENV['dbpass'], :dbname => ENV['dbname'], :host => ENV['hostid'], :port => ENV['portid'])
+# conn = PG::Connection.open(:user => ENV['dbuser'], :password => ENV['dbpass'], :dbname => ENV['dbname'], :host => ENV['hostid'], :port => ENV['portid'])
 
 def prepare_statements(conn)
 	conn.prepare("newuser4", "insert into user_info (uuid, user_id, user_pass) values($1, $2, $3)")
 	conn.prepare("cons", "insert into contact_info (names, address, phone, owner) values($1, $2, $3, $4)")
 end
-prepare_statements(conn)
+# prepare_statements(conn)
 
 get '/' do
 
@@ -20,28 +20,28 @@ end
 post '/account' do
 	user = params[:user]
 	pass = params[:pass]
-	res = conn.exec("SELECT * FROM user_info WHERE user_id = '#{user}' AND user_pass = '#{pass}' ")
-	res.each do |n|
-		session[:id] = n['uuid']
-		session[:user_name] = n['user_id']
+	# res = conn.exec("SELECT * FROM user_info WHERE user_id = '#{user}' AND user_pass = '#{pass}' ")
+	# res.each do |n|
+	# 	session[:id] = n['uuid']
+	# 	session[:user_name] = n['user_id']
 		redirect '/profile'
-	end
-		erb :dashboard, locals:{error: "", msg: ""}
+	# end
+		# erb :dashboard, locals:{error: "", msg: ""}
 end
 
 post '/newuser' do
-	conn.exec_prepared('newuser4', [SecureRandom.uuid, params[:new_user] , params[:new_pass]])
+	# conn.exec_prepared('newuser4', [SecureRandom.uuid, params[:new_user] , params[:new_pass]])
 	
 	msg = "Account Created"
 	erb :dashboard, locals:{msg: "Account Created"}
 end
 
 get '/profile' do
-	res = conn.exec("SELECT * FROM contact_info WHERE owner = '#{session[:id]}'")
+	# res = conn.exec("SELECT * FROM contact_info WHERE owner = '#{session[:id]}'")
 	res_arr = []
-	res.each do |r|
-		res_arr << r
-	end
+	# res.each do |r|
+	# 	res_arr << r
+	# end
 
 
 	username = session[:user_name]
@@ -49,43 +49,43 @@ get '/profile' do
 end
 
 post '/contacts' do
-	person = params[:person]
-	address = params[:address]
-	phone = params[:phone]
-	conn.exec_prepared('cons', [params[:person], params[:address], params[:phone], session[:id]])
+	# person = params[:person]
+	# address = params[:address]
+	# phone = params[:phone]
+	# conn.exec_prepared('cons', [params[:person], params[:address], params[:phone], session[:id]])
 	redirect '/profile'
 end
 
 post '/delete_con' do
-	id = params[:id]
+	# id = params[:id]
 
-	conn.exec("DELETE FROM contact_info WHERE id = '#{params[:id]}'")
+	# conn.exec("DELETE FROM contact_info WHERE id = '#{params[:id]}'")
 	redirect '/profile'
 end
 
 get '/update_con' do
-	names = names || ""
-	phone = phone || ""
-	address = address || ""
+	# names = names || ""
+	# phone = phone || ""
+	# address = address || ""
 	res_arr = res_arr || []
-	owner = session[:id]
-	id = params[:id2]
+	# owner = session[:id]
+	# id = params[:id2]
 
-	conn.exec("UPDATE contacts SET names = '#{names}', phone = '#{phone}', address = '#{address}' WHERE id = '#{id}'")
-	res = conn.exec("SELECT * FROM contact_info WHERE owner = '#{session[:id]}'")
-	res_arr = []
-	res.each do |r|
-		res_arr << r
-	end
+	# conn.exec("UPDATE contacts SET names = '#{names}', phone = '#{phone}', address = '#{address}' WHERE id = '#{id}'")
+	# res = conn.exec("SELECT * FROM contact_info WHERE owner = '#{session[:id]}'")
+	# res_arr = []
+	# res.each do |r|
+	# 	res_arr << r
+	# end
 
 	erb :phonebook, locals:{res_arr: res_arr}
 end
 
 post '/update_con' do
-	id = id || ""
-	names = params[:names2]
-	phone = params[:phone2]
-	address = params[:address2]
+	# id = id || ""
+	# names = params[:names2]
+	# phone = params[:phone2]
+	# address = params[:address2]
 
 	redirect '/profile'
 end
